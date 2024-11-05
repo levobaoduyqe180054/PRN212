99980f813bda3b946c3c736f67055ebb3f12a3bb
Lê Võ Bảo Duy <145436410+levobaoduyqe180054@users.noreply.github.com>
2024-11-05 13:03:34 +0700

    SettingsWindow.xaml.cs

--- PRN212/Settings page/SettingsWindow.xaml.cs
+++ PRN212/Settings page/SettingsWindow.xaml.cs
@@ -1,27 +1,149 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using PRN212.Help_page;
+using PRN212.Authentication_page;
+using PRN212.Calendar_Page;
+using PRN212.Home;
+using PRN212.Customize_Page;
+using PRN212.DAL.Models;
+using PRN212.BLL.Services;
+using Microsoft.Win32;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+using System.Linq;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
+using PRN212.Tasks_page;
 
 namespace PRN212.Settings_page
 {
-    /// <summary>
-    /// Interaction logic for SettingsWindow.xaml
-    /// </summary>
     public partial class SettingsWindow : Window
     {
         public SettingsWindow()
         {
             InitializeComponent();
+
+            // Display the current User's name and email
+            UsernameTextBlock.Text = CurrentUser.User.Username;
+            EmailTextBlock.Text = CurrentUser.User.Email;
+        }
+
+        private void Home_Click(object sender, RoutedEventArgs e)
+        {
+            Home mainWindow = new Home();
+            mainWindow.Show();
+            this.Close();
+        }
+
+        private void Calendar_Click(object sender, RoutedEventArgs e)
+        {
+            CalendarPage mainWindow = new CalendarPage();
+            mainWindow.Show();
+            this.Close();
+        }
+
+        private void TaskWindow_Click(object sender, RoutedEventArgs e)
+        {
+            TasksPage mainWindow = new TasksPage();
+            mainWindow.Show();
+            this.Close();
+        }
+
+        private void Customize_Click(object sender, RoutedEventArgs e)
+        {
+            CustomizePage mainWindow = new CustomizePage();
+            mainWindow.Show();
+            this.Close();
+        }
+
+        private void SettingWindow_Click(object sender, RoutedEventArgs e)
+        {
+            SettingsPage mainWindow = new SettingsPage();
+            mainWindow.Show();
+            this.Close();
+        }
+
+        private void HelpWindow_Click(object sender, RoutedEventArgs e)
+        {
+            HelpPage mainWindow = new HelpPage();
+            mainWindow.Show();
+            this.Close();
+        }
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void Save_Click(object sender, RoutedEventArgs e)
+        {
+            // Check if the text boxes are empty
+            if (string.IsNullOrWhiteSpace(tb_name.Text) ||
+                string.IsNullOrWhiteSpace(tb_email.Text) ||
+                string.IsNullOrWhiteSpace(tb_newPassword.Password) ||
+                string.IsNullOrWhiteSpace(tb_password.Password))
+            {
+                MessageBox.Show("Please fill in all fields before saving changes.");
+                return;
+            }
+
+            // Find the current User in the list
+            UserData user = users.Where(u => u.Username == CurrentUser.User.Username).FirstOrDefault();
+
+            // If the user was found, verify the old password
+            if (user != null)
+            {
+                if (user.Password != tb_password.Password)
+                {
+                    MessageBox.Show("The old password is incorrect.");
+                    return;
+                }
+
+                user.Username = tb_name.Text;
+                user.Email = tb_email.Text;
+                user.Password = tb_newPassword.Password;
+
+                // Update properties of the UserData class
+                CurrentUser.User.Username = tb_name.Text;
+                CurrentUser.User.Email = tb_email.Text;
+            }
+
+            // Save the JSON file
+            json = JsonConvert.SerializeObject(users, Formatting.Indented);
+            File.WriteAllText(jsonFilePath, json);
+
+            SettingsPage mainWindow = new SettingsPage();
+            mainWindow.Show();
+            this.Close();
+        }
+
+        private void EndSession_Click(object sender, RoutedEventArgs e)
+        {
+            LoginPage mainWindow = new LoginPage();
+            mainWindow.Show();
+            this.Close();
+        }
+
+        private void tb_name_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+        }
+
+        private void PasswordBox_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void ChangeImage_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Image files (*.png;*.jpeg;*.jpg;*.bmp)|*.png;*.jpeg;*.jpg;*.bmp|All files (*.*)|*.*";
+            if (openFileDialog.ShowDialog() == true)
+            {
+                Uri fileUri = new Uri(openFileDialog.FileName);
+                userImage.Source = new BitmapImage(fileUri);
+
+                // Maintain image dimensions
+                userImage.Width = 95;
+                userImage.Height = 101;
+            }
         }
     }
 }
